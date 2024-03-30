39936c30554bfb7e4ebbfe3ebe2e17a90e367e1d
Brendan Freeman <bfreeman0@gmail.com>
2024-03-30 22:51:16 +0000

    30 Mar 2024, 22:51

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import "./style.css";
-import { buildTree } from "./modules/buildTree";
 import prettyPrint from "./modules/prettyPrint";
 import { createTree } from "./modules/Tree";
 
 const arr = [2, 89, 23, 40]; // => 2, 23, 40, 89
 const arrLength = arr.length; // => 4
-
-const root = buildTree(arr, 0, arrLength - 1);
 
 const theTree = createTree(arr);
 console.log();
