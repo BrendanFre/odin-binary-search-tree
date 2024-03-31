59f87ac395908c929e34e8fdecdfea2cd4c4c992
Brendan Freeman <bfreeman0@gmail.com>
2024-03-31 14:38:39 +0000

    31 Mar 2024, 15:38

--- src/modules/deleteNode.ts
+++ src/modules/deleteNode.ts
@@ -3,56 +3,53 @@ import { insertTree } from "./insertTree";
 export function deleteNode(number: any, tree: any): any {
   if (tree === null || tree === undefined) {
     return null;
-  } else if (tree.data == number) {
+  } else if (tree.leftChild.data == number) {
+    checkBranches(tree.leftChild);
     console.log("tree found for deletion");
-    deletionNode(number, tree);
   } else if (tree != undefined || tree != null) {
     return (
       deleteNode(number, tree.leftChild) || deleteNode(number, tree.rightChild)
     );
   }
-
-  function deletionNode(number: any, tree: any) {
-    if (tree.leftChild == null && tree.rightChild == null) {
-      console.log(`Node containing ${number} has been deleteed.`);
-      return (tree = null);
-    } else if (tree.leftChild != null) {
-      console.log("left child is an object");
-      if (typeof tree.rightChild == null) {
-        console.log("update left");
-        tree = tree.leftChild;
-      } else if (typeof tree.rightChild == "object") {
-        let __return;
-        ({ __return, tree } = moveBranches(tree));
-        return __return;
-      }
+  function moveBranches(tree: any) {
+    console.log(`Two branches`);
+    const firstChild = tree.leftChild;
+    const secondChild = tree.rightChild;
+    tree.data = null;
+    if (firstChild.data > secondChild.data) {
+      tree.data = firstChild.data;
+      tree.firstChild = firstChild.firstChild;
+      tree.secondChild = firstChild.secondChild;
     } else {
-      console.log("delete right");
-      tree.data = tree.rightChild.data;
-      tree.leftChild = tree.rightChild.leftChild;
-      tree.rightChild = tree.rightChild.leftChild;
+      const node = tree.secondChild;
+      tree.firstChild = null;
+      tree.secondChild = null;
+      // console.log(node);
+      // // tree.data = node.data;
+      // tree.firstChild = firstChild;
+      // tree.secondChild = secondChild;
+    }
+    tree = null;
+    insertTree(firstChild, tree) || insertTree(secondChild, tree);
+    return { __return: (tree = null), tree };
+  }
+
+  /**
+   *
+   * @param node takes any object
+   * @returns node with the correct children
+   */
+  function checkBranches(node: object) {
+    if ("data" in node && "leftChild" in node && "rightChild" in node) {
+      if (node.leftChild != null) {
+        if (node.rightChild != null) {
+          // Right and left child are not empty
+          throw new Error("not prepped");
+        } else {
+          // Right child is empty
+          return node.leftChild;
+        }
+      }
     }
   }
-}
-function moveBranches(tree: any) {
-  console.log(`Two branches`);
-  const firstChild = tree.leftChild;
-  const secondChild = tree.rightChild;
-  tree.data = null;
-  if (firstChild.data > secondChild.data) {
-    tree.data = firstChild.data;
-    tree.firstChild = firstChild.firstChild;
-    tree.secondChild = firstChild.secondChild;
-  } else {
-    const node = tree.secondChild;
-    tree.firstChild = null;
-    tree.secondChild = null;
-    // console.log(node);
-    // // tree.data = node.data;
-    // tree.firstChild = firstChild;
-    // tree.secondChild = secondChild;
-  }
-  tree = null;
-  insertTree(firstChild, tree) || insertTree(secondChild, tree);
-  return { __return: (tree = null), tree };
 }
