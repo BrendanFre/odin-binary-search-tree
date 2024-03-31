7e38793068423186cf8e8efdc60d37805d4c3586
Brendan Freeman <bfreeman0@gmail.com>
2024-03-31 13:52:03 +0000

    31 Mar 2024, 14:52

--- src/modules/deleteNode.ts
+++ src/modules/deleteNode.ts
@@ -1,1 +1,30 @@
+export function deleteNode(number: any, tree: any): any {
+  if (tree === null || tree === undefined) {
+    return null;
+  } else if (tree.data == number) {
+    console.log("tree found");
+    deletion(number, tree);
+  } else if (tree != undefined || tree != null) {
+    return (
+      deleteNode(number, tree.leftChild) || deleteNode(number, tree.rightChild)
+    );
+  }
 
+  function deletion(number: any, tree: any) {
+    if (tree.leftChild == null && tree.rightChild == null) {
+      console.log(`Node containing ${number} has been deleteed.`);
+      return (tree = null);
+    } else if (typeof tree.leftChild == "object") {
+      if (typeof tree.leftChild == null) {
+        tree = tree.leftChild;
+      } else {
+        const firstChild = tree.leftChild;
+        const secondChild = tree.rightChild;
+        tree = undefined;
+        tree.insert(firstChild, tree);
+        tree.insert(secondChild, tree);
+        return (tree = null);
+      }
+    }
+  }
+}
