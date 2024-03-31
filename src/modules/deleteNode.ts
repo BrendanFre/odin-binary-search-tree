3a57d7b38bf840bb3ec74e298851750903432586
Brendan Freeman <bfreeman0@gmail.com>
2024-03-31 12:53:16 +0000

    31 Mar 2024, 13:53

--- src/modules/deleteNode.ts
+++ src/modules/deleteNode.ts
@@ -2,6 +2,7 @@ export function deleteNode(number: any, tree: any): any {
   if (tree === null || tree === undefined) {
     return null;
   } else if (tree.data == number) {
+    console.log("tree found");
     return (tree = null);
   } else if (tree != undefined || tree != null) {
     return (
